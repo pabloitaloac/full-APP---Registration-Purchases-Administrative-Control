fdc3614b71c3b81d27e2ac4535f5171f04af9d2e
pabloitaloac <pablo_italo12@hotmail.com>
2022-11-13 17:20:51 -0300

    0.5

--- modules/express.js
+++ modules/express.js
@@ -25,8 +25,13 @@ app.get("/adm/users", async (req, res) => {
     app.post('/adm/users', async (req,res)=>{
 
       //Filter by email
-        var filterEmail = req.body.emailFilter
-            //If email is whrited, search in DB
+        var filterEmail =  req.body.emailFilter
+      //Filter by firstName
+        var filterFirstName =  req.body.firstNameFilter
+      //Filter by firstName
+        var filterLastName =  req.body.lastNameFilter
+
+            // If email is whrited, search in DB
             if(filterEmail.length > 0){
                 //var to search
                 var emailAtDB = await UserModel.find({
@@ -34,24 +39,72 @@ app.get("/adm/users", async (req, res) => {
                 })
                     //if exist any serched
                     if(emailAtDB.length > 0){  
-                         var ijdeqw = "gh7ugh79ed321"
-                      // res.send(` ${typefilterEmail}`)
+                         
                     res.render("allUsers", { users: emailAtDB} )
-
+ 
                     } 
                     //if not exist
                     else if(emailAtDB.length == 0){
-                      res.send(` Email pesquisado não encontrado `)
-                    }
-            }     
-            //If not, create var with 'null'
-            else {
-              res.send(`Digite um email para pesquisar`)
-            }
+                      
+                      // res.end()
+                      
 
  
-    })
 
+                      // res.send(` Email pesquisado não encontrado `)
+                    }
+            }   
+
+            // If Last Name is whrited, search in DB   
+            else if(filterLastName.length > 0){
+              //var to search
+              var lastNameAtDB = await UserModel.find({
+                "lastName": filterLastName
+              })
+                  //if exist any serched
+                  if(lastNameAtDB.length > 0){  
+                       
+                  res.render("allUsers", { users: lastNameAtDB} )
+    
+                  } 
+                  //if not exist
+                  else if(lastNameAtDB.length == 0){
+                    res.send(` Último Nome pesquisado não encontrado `)
+                  }
+          }
+
+          // If First Name is whrited, search in DB   
+          else if(filterFirstName.length > 0){
+            //var to search
+            var firstNameAtDB = await UserModel.find({
+              "firstName": filterFirstName
+            })
+                //if exist any serched
+                if(firstNameAtDB.length > 0){  
+                     
+                res.render("allUsers", { users: firstNameAtDB} )
+  
+                } 
+                //if not exist
+                else if(firstNameAtDB.length == 0){
+                  res.send(` Primeiro Nome pesquisado não encontrado `)
+                }
+        }
+          
+              
+
+    })
+     
+        
+      
+
+
+
+
+            
+ 
+      
+ 
 
 // app.get('/users', async (req,res)=>{
 //     try{
@@ -75,7 +128,7 @@ app.get("/adm/users/:id", async (req, res) => {
     return res.status(500).send(error.message);
   }
 });
-
+ 
 
 
 //To update user data
@@ -92,83 +145,117 @@ app.get("/adm/users/:id", async (req, res) => {
 // })
 
 //To delete user
-// app.delete('/adm/users/delete/:id', async(req,res)=>{
-//     try{
-//         const id = req.params.id
-//         const user = await UserModel.findByIdAndRemove(id)
+app.post('/adm/delete/users/:id', async(req,res)=>{
+    try{
+        const id = req.params.id
+        const user = await UserModel.findByIdAndRemove(id)
 
-//         return res.status(200).json(user)
-//     }
-//     catch(error){
-//         res.status(500).send(error.message)
-//     }
-// })
+        return res.status(200).render('userDeleted', {userByID: user})
+    }
+    catch(error){
+        res.status(500).send(error.message)
+    } 
+})
 
 //criar usuários
-//utiliza Schema em outro arquivo para fazer
-//  .post (não é .get)
-//status 201 é padrão para confirmar criação de usuários
+      //utiliza Schema em outro arquivo para fazer
+      //status 201 é padrão para confirmar criação de usuários
 
-//Page of ADD users - redirect to url to add effectivity users
-app.get("/adm/add/users", async (req, res) => {
-  res.render("addUser");
-});
+      //Page of ADD users - redirect to url to add effectivity users
+      app.get("/adm/add/users", async (req, res) => {
+        res.render("addUser");
+      });
 
-// ADD - Using url to receive json
-app.post("/adm/add/single_user", async (req, res) => {
-  try {
+      // ADD - Using url to receive json
+      app.post("/adm/add/single_user", async (req, res) => {
+        try {
 
-    var emailClient = req.body.email
-            //If email is whrited, search in DB
-            if(emailClient.length > 0){
-              var validateEmailDB = await UserModel.findOne({
-                "email": emailClient
-              })      
-            }
-            //If not, create var with 'null'
-            else {
-              var validateEmailDB = null
-            } 
+          var emailClient = req.body.email
+                  //If email is whrited, search in DB
+                  if(emailClient.length > 0){
+                    var validateEmailDB = await UserModel.findOne({
+                      "email": emailClient
+                    })      
+                  }
+                  //If not, create var with 'null'
+                  else {
+                    var validateEmailDB = null
+                  } 
 
-    var firstNameClient = req.body.firstName
-    var lastNameClient = req.body.lastName
-    var passwordClient = req.body.password
-    
-    // NÃO ESTÁ INDO ?/
-    var regex = /^(?=(?:.*?[A-Z]){3})(?=(?:.*?[0-9]){2})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/
-    //Is all data complete? - Todos os campos escritos?
-    if(firstNameClient.length>0 && lastNameClient.length>0 && emailClient.length>0 && passwordClient.length>0){
-      
-              // If yes, validate email and continue
-              if (validateEmailDB != null && emailClient == validateEmailDB.email){
-                res.send(`Email Existente!`)
-              }
-              //Validate password - mínimo 3 caracteres em maiúsculo, 2 números e 1 caractere especial
-              else if(passwordClient.length < 8){
-                  res.send(` senha deve conter no minímo 8 digitos!`)
-              }
-              else if(!regex.exec(passwordClient))
-              {
-                  res.send(`A senha deve conter no mínimo 3 caracteres em maiúsculo, 2 números e 1 caractere especial!`)
-              }
-              else {
-                  //if all OK, creat new user
-                  const user = await UserModel.create({
-                  firstName: req.body.firstName,
-                  lastName: req.body.lastName,
-                  email: req.body.email,
-                  password: req.body.password,
-                  });
-                res.status(201).send(`Cadastro criado com email novo`)
-              }
-    }
-    else{
-      res.send(`Preencha TODOS os campos`)
-    }                            
-  } catch (error) {
-    res.status(500).send(`ERRO: ${error.message}`);
+          var firstNameClient = req.body.firstName
+          var lastNameClient = req.body.lastName
+          var passwordClient = req.body.password
+          
+          // NÃO ESTÁ INDO ?/
+          var regex = /^(?=(?:.*?[A-Z]){3})(?=(?:.*?[0-9]){2})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/
+          //Is all data complete? - Todos os campos escritos?
+          if(firstNameClient.length>0 && lastNameClient.length>0 && emailClient.length>0 && passwordClient.length>0){
+            
+                    // If yes, validate email and continue
+                    if (validateEmailDB != null && emailClient == validateEmailDB.email){
+                      res.send(`Email Existente!`)
+                    }
+                    //Validate password - mínimo 3 caracteres em maiúsculo, 2 números e 1 caractere especial
+                    else if(passwordClient.length < 8){
+                        res.send(` senha deve conter no minímo 8 digitos!`)
+                    }
+                    else if(!regex.exec(passwordClient))
+                    {
+                        res.send(`A senha deve conter no mínimo 3 caracteres em maiúsculo, 2 números e 1 caractere especial!`)
+                    }
+                    else {
+                        //if all OK, creat new user
+                        const user = await UserModel.create({
+                        firstName: req.body.firstName,
+                        lastName: req.body.lastName,
+                        email: req.body.email,
+                        password: req.body.password,
+                        });
+                      res.status(201).send(`Cadastro criado com email novo`)
+                    }
+          } 
+          else{
+            res.send(`Preencha TODOS os campos`)
+          }                            
+        } catch (error) {
+          res.status(500).send(`ERRO: ${error.message}`);
+        }
+      })
+   
+
+
+
+app.get('/teste', async (req,res)=>{
+  try{
+    res.render('teste')
+    } catch {
+      res.send('erro')
+    } 
+
+  
+
+})
+
+app.post('/teste', async (req,res)=>{
+  try{
+    var testando1 = req.body.testing1
+    var testando2 = req.body.testing2
+
+    // res.send(testando1())
+    await res.render('testePost', {'tst1': testando1, 'tst2': testando2})
+  } catch {
+    res.send('erro')
   }
 })
+
+
+
+
+ 
+
+
+
+
 
 const port = 8081;
 app.listen(port, () => {
